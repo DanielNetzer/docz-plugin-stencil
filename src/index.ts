f2a87e651d0232ff2cc19e39cff1694077a2ff36
DanielNetzer <daniel.netzer7@gmail.com>
2019-03-23 22:01:03 +0200

    removed unused files, updated gitignore, updated readme

--- src/index.ts
+++ src/index.ts
@@ -1,141 +1,134 @@
-import { createPlugin } from "docz-core";
-import * as fs from "fs";
-import * as path from "path";
+import { createPlugin } from 'docz-core'
+import * as fs from 'fs'
+import * as path from 'path'
 
-const OUTPUT_PATH = "docs-copy";
+const OUTPUT_PATH = 'docs'
 
 function fromDir(
   startPath: string,
   filter: RegExp,
-  callback: (filename: string) => void
-) {
+  callback: (filename: string, err: any) => void
+): void {
   if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath);
-    return;
+    return
   }
 
-  var files = fs.readdirSync(startPath);
-  for (var i = 0; i < files.length; i++) {
-    var filename = path.join(startPath, files[i]);
-    var stat = fs.lstatSync(filename);
+  const files = fs.readdirSync(startPath)
+  for (const file of files) {
+    const filename = path.join(startPath, file)
+    const stat = fs.lstatSync(filename)
 
     if (stat.isDirectory()) {
-      fromDir(filename, filter, callback); //recurse
-    } else if (filter.test(filename)) callback(filename);
+      fromDir(filename, filter, callback) // recurse
+    } else if (filter.test(filename)) callback(filename, null)
   }
 }
 
-function getComponentsModules(startPath: string): string[] | undefined {
+function getComponentsModules(startPath: string): string[] {
   if (!fs.existsSync(startPath)) {
-    console.error(`Directory ${startPath} doesn't exist!`);
-    return;
+    console.error(`Directory ${startPath} doesn't exist!`)
+    return []
   }
 
   const componentPaths = fs
     .readdirSync(startPath)
-    .map(f => path.join(startPath, f));
-  return componentPaths;
+    .map(f => path.join(startPath, f))
+  return componentPaths
 }
 
 export interface DoczStencilPluginOptions {
-  outputPath: string;
+  outputPath: string
 }
 
 export const stencil = (opts?: DoczStencilPluginOptions) => {
-  const outputPath = opts.outputPath ? opts.outputPath : OUTPUT_PATH;
+  const options = opts
+    ? opts
+    : {
+        outputPath: OUTPUT_PATH,
+      }
+
   return createPlugin({
-    // modifyBundlerConfig: (config, dev, args) => {
-    //   console.log("modifyBundlerConfig");
-    //   return config;
-    // },
-    // modifyBabelRc: (babelrc, args) => {
-    //   console.log("modifyBabelRc");
-    //   return babelrc;
-    // },
-    // onServerListening: server => {
-    //   console.log("onServerListening");
-    // },
-    // onPreBuild: () => {
-    //   console.log("onPreBuild");
-    // },
-
-    // onPostBuild: args => {
-    //   console.log("onPostBuild");
-    // },
-    // onPostRender: () => {
-    //   console.log("onPostRender");
-    // },
-    // onPreRender: () => {
-    //   console.log("onPreRender");
-    // },
-    // onCreateApp: app => {
-    //   console.log("onCreateApp");
-    // },
     modifyFiles: files => {
-      console.log("modifyFiles");
-      const componentsModules = getComponentsModules("src/components");
+      const componentsModules = getComponentsModules('src/components')
       componentsModules.forEach(cmpMdlPath => {
-        let cmpName = cmpMdlPath.split("\\").pop();
-        let mdContent: string[] = [];
-        let playgroundContent: string[] = [];
-        fromDir(cmpMdlPath, /\.md$/, filenameWithPath => {
-          let filename = filenameWithPath
-            .split("\\")
-            .pop()
-            .split(".")[0];
+        const cmpName = cmpMdlPath.split('\\').pop()
+        let mdContent: string[] = []
+        let playgroundContent: string[] = []
 
-          if (filename === "playground") {
+        fromDir(cmpMdlPath, /\.md$/, (filenameWithPath, err) => {
+          if (err) throw new Error(err)
+
+          const filePath = filenameWithPath.split('\\')
+          let fileWIthExtension: string
+          let filename: string
+
+          if (filePath) {
+            fileWIthExtension = filePath.pop() as string
+          } else {
+            return
+          }
+
+          if (fileWIthExtension) {
+            filename = fileWIthExtension.split('.')[0]
+          } else {
+            return
+          }
+
+          if (filename === 'playground') {
             playgroundContent = fs
-              .readFileSync(filenameWithPath, { encoding: "utf8" })
-              .split("\n");
-            return;
+              .readFileSync(filenameWithPath, { encoding: 'utf8' })
+              .split('\n')
+            return
           }
 
-          if (filename === "readme") {
+          if (filename === 'readme') {
             mdContent = fs
-              .readFileSync(filenameWithPath, { encoding: "utf8" })
-              .split("\n");
-            return;
+              .readFileSync(filenameWithPath, { encoding: 'utf8' })
+              .split('\n')
+            return
           }
-        });
+        })
 
-        const doczMdHeader = ["", "---", `name: ${cmpName}`, "---", ""];
-        const doczPGContent = [
-          "",
-          "import { Playground } from 'docz';",
-          "import { defineCustomElements } from './loader';",
-          "defineCustomElements(window);",
-          "",
-          "<Playground>",
-          ...playgroundContent,
-          "</Playground>",
-          ""
-        ];
+        const doczMdHeader = ['', '---', `name: ${cmpName}`, '---', '']
+        let doczPGContent: string[] = []
 
-        mdContent.unshift(...doczMdHeader);
+        if (playgroundContent) {
+          doczPGContent = [
+            '',
+            "import { Playground } from 'docz';",
+            "import { defineCustomElements } from './loader';",
+            'defineCustomElements(window);',
+            '',
+            '<Playground>',
+            ...playgroundContent,
+            '</Playground>',
+            '',
+          ]
+        }
+
+        mdContent.unshift(...doczMdHeader)
 
         // remove playground title
         const playGrndInsertionIndex = mdContent.findIndex(ctntLine =>
-          ctntLine.includes("Auto Generated Below")
-        );
+          ctntLine.includes('Auto Generated Below')
+        )
 
         // insert the playground content
-        mdContent.splice(playGrndInsertionIndex + 1, 0, ...doczPGContent);
+        mdContent.splice(playGrndInsertionIndex + 1, 0, ...doczPGContent)
 
-        const doczMdxContent = mdContent.join("\n");
+        const doczMdxContent = mdContent.join('\n')
 
         // create new file with playground content, name it after the component name
-        fs.writeFileSync(`${outputPath}/${cmpName}.mdx`, doczMdxContent);
-      });
-      return files;
+        fs.writeFileSync(`${options.outputPath}/${cmpName}.mdx`, doczMdxContent)
+      })
+      return files
     },
     setConfig: config => {
-      console.log("setConfig");
-      config.files = '**/*.mdx';
-      config.codeSandbox = false;
-      config.typescript = true;
-      config.propsParser = false;
-      return config;
-    }
-  });
-};
+      config.files = '**/*.mdx'
+      config.codeSandbox = false
+      config.typescript = true
+      config.propsParser = false
+      return config
+    },
+  })
+}
